5ec124e89baba7e2c79e39bcd38cfdbc273746a3
ppitjine <chaoweinung@ChaodeMacBook-Air.local>
2014-12-21 20:51:15 +0800

    新增QRcode SDK
    僅安裝ＳＤＫ尚未使用

--- search_demo/build/iphone/Classes/ApplicationMods.m
+++ search_demo/build/iphone/Classes/ApplicationMods.m
@@ -1,15 +1,37 @@
 /**
  * Appcelerator Titanium Mobile
- * This is generated code. Do not modify. Your changes will be lost.
+ * Copyright (c) 2009-2014 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ *
+ * WARNING: This is generated code. Do not modify. Your changes *will* be lost.
  */
-#import "ApplicationMods.h"
 
+#import "ApplicationMods.h"
 
 @implementation ApplicationMods
 
 + (NSArray*) compiledMods
 {
-	return nil;
+	NSMutableArray *modules = [NSMutableArray array];
+
+	
+		[modules addObject:[NSDictionary
+			dictionaryWithObjectsAndKeys:@"barcode",
+			@"name",
+			@"ti.barcode",
+			@"moduleid",
+			@"1.8.3",
+			@"version",
+			@"fe2e658e-0eaf-44a6-b6d1-c074d6b986a3",
+			@"guid",
+			@"",
+			@"licensekey",
+			nil
+		]];
+		
+
+	return modules;
 }
 
 @end

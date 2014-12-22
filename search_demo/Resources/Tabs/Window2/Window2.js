b214ef7dc0d8b3794f4f2dcc37e373eb6aacdacb
ppitjine <chaoweinung@ChaodeMacBook-Air.local>
2014-12-22 15:37:06 +0800

    新增會員登入部分
    有error
    新增
    1.UI
    2.php登入
    3.登入檢查 獲取資料（error）

--- search_demo/Resources/Tabs/Window2/Window2.js
+++ search_demo/Resources/Tabs/Window2/Window2.js
@@ -2,6 +2,76 @@ exports.Window2 = function (g_Vars)
 {
 // Creation - GUI DO NOT TOUCH
 // [an:PartOfTab][true][ta:PartOfTab][true][ph:PartOfTab][true][pa:PartOfTab][true][mw:PartOfTab][true][an:icon][/images/tab-duplicate-3.ico][ta:icon][/images/tab-duplicate-3.ico][ph:icon][/images/tab-duplicate-3.ico][pa:icon][/images/tab-duplicate-3.ico][mw:icon][/images/tab-duplicate-3.ico][an:name][Window2][ta:name][Window2][ph:name][Window2][pa:name][Window2][mw:name][Window2][an:backgroundColor][#ffffff][ta:backgroundColor][#ffffff][ph:backgroundColor][#ffffff][pa:backgroundColor][#ffffff][mw:backgroundColor][#ffffff][an:title][會員登入][ta:title][會員登入][ph:title][會員登入][pa:title][會員登入][mw:title][會員登入]
+
+
+var username = Titanium.UI.createTextField({
+    color:'#336699',
+    top:10,
+    left:10,
+    width:300,
+    height:40,
+    hintText:'Username',
+    keyboardType:Titanium.UI.KEYBOARD_DEFAULT,
+    returnKeyType:Titanium.UI.RETURNKEY_DEFAULT,
+    borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED
+});
+
+ 
+var password = Titanium.UI.createTextField({
+    color:'#336699',
+    top:60,
+    left:10,
+    width:300,
+    height:40,
+    hintText:'Password',
+    passwordMask:true,
+    keyboardType:Titanium.UI.KEYBOARD_DEFAULT,
+    returnKeyType:Titanium.UI.RETURNKEY_DEFAULT,
+    borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED
+});
+
+ 
+var loginBtn = Titanium.UI.createButton({
+    title:'Login',
+    top:110,
+    width:90,
+    height:35,
+    borderRadius:1,
+    font:{fontFamily:'Arial',fontWeight:'bold',fontSize:14}
+});
+
+var loginReq = Titanium.Network.createHTTPClient();
+ loginReq.onload = function()
+	{
+	    var json = this.responseText;
+	    var response = JSON.parse(json);
+	    if (response.logged == true)
+	    {
+	        alert("Welcome " + response.name);
+	    }
+	    else
+	    {
+	        alert(response.message);
+	    }
+	};
+loginBtn.addEventListener('click',function(e)
+{
+    if (username.value != '' && password.value != '')
+    {
+        loginReq.open("POST","http://hellosearch.esy.es/ti_search/post_auth.php");
+        var params = {
+            username: username.value,
+            password: Ti.Utils.md5HexDigest(password.value)
+        };
+        loginReq.send(params);
+    }
+    else
+    {
+        alert("Username/Password are required");
+    }
+});
+
+
 var win;
 win = Titanium.UI.createWindow({
 PartOfTab : true,
@@ -20,8 +90,9 @@ win.addEventListener ('close', function (e) {
 });
 // Window2_close - END
 
-
-
+win.add(username);
+win.add(password);
+win.add(loginBtn);
 
 // Startup - GUI DO NOT TOUCH
 var aoChildWindows = undefined;
